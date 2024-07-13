7a625c2d58efef953555d90aaf2c835d15086ea6
Uday Gupta <62444103+udaygupta188@users.noreply.github.com>
2024-07-13 20:28:30 +0530

    db config fixed

--- configs/db.config.js
+++ configs/db.config.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async (DB) => {
     console.log('Connecting to MongoDB...');
+    const defaultDB = process.env.MONGO_URI;
+    const databaseURL = DB || defaultDB;
     try {
-        const conn = await mongoose.connect(DB);
+        const conn = await mongoose.connect(databaseURL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
